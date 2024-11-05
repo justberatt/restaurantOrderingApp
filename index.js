888f5031c5062bda1b66d7cb1be140c0f513ebdd
Berat Sakipi <berat.sakipi@gmail.com>
2024-11-05 23:54:14 +0100

    Displaying the menu items on the page. What's left is the styling and I am done with first phase, the 'default-state' phase.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import { menuArray } from './data.js'
 
-console.log(menuArray)
+const container = document.querySelector('#container');
+const menu = menuArray.map(item => {
+    return `
+        <li class="menu-item">
+            <span id=${item.name.toLowerCase()}-emoji class="emoji">${item.emoji}</span>
+            <h2 id="${item.name.toLowerCase()}" class="item-name">${item.name}</h2>
+            <p class="ingredients">${item.ingredients.join(' ')}</p>
+            <p class="price">${item.price}</p>
+        </li>
+    `
+}).join('')
+
+container.innerHTML = menu;
