85688b3f9a914e174d9a40f0c1674e59b4f4e1a0
Berat Sakipi <berat.sakipi@gmail.com>
2024-11-07 20:29:46 +0100

    Added the CSS

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import { menuArray } from './data.js'
 
-const container = document.querySelector('#container');
-const menu = menuArray.map(item => {
+const menu = document.querySelector('#menu');
+const menuItems = menuArray.map(item => {
     return `
         <li class="menu-item">
-            <span id=${item.name.toLowerCase()}-emoji class="emoji">${item.emoji}</span>
-            <h2 id="${item.name.toLowerCase()}" class="item-name">${item.name}</h2>
-            <p class="ingredients">${item.ingredients.join(' ')}</p>
-            <p class="price">${item.price}</p>
+            <div class="emoji-container">
+                <div id=${item.name.toLowerCase()}-emoji class="emoji">${item.emoji}</div>
+            </div>
+            <div class="item-info">
+                <h2 id="${item.name.toLowerCase()}" class="item-name">${item.name}</h2>
+                <p class="ingredients">${item.ingredients.join(' ')}</p>
+                <h4 class="price">$${item.price}</h4>
+            </div>
+            <button class="add-to-card__btn" id="add-to-card__btn">+</button>
         </li>
+        <hr>
     `
 }).join('')
 
-container.innerHTML = menu;
+menu.innerHTML = menuItems;
