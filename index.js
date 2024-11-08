72d53a689538b2806d11537ef33fc21bfaf4c311
Berat Sakipi <berat.sakipi@gmail.com>
2024-11-08 23:56:27 +0100

    Added the precheckout list. Working on the JavaScript

--- index.js
+++ index.js
@@ -12,10 +12,33 @@ const menuItems = menuArray.map(item => {
                 <p class="ingredients">${item.ingredients.join(' ')}</p>
                 <h4 class="price">$${item.price}</h4>
             </div>
-            <button class="add-to-card__btn" id="add-to-card__btn">+</button>
+            <button class="add-to-card__btn" id="${item.id}">+</button>
         </li>
         <hr>
     `
 }).join('')
 
 menu.innerHTML = menuItems;
+
+menu.addEventListener('click', (e) => {
+    handleAddToCartClick(e)
+})
+
+const handleAddToCartClick = (e) => {
+    console.log("targetID: ", e.target.id)
+    menuArray.forEach(item => {
+        if (e.target.id === item.id) {
+            console.log(item.id)
+        }
+    })
+    // const preCheckoutList = menuArray.map(item => {
+    //     return `
+    //         <li>
+    //             <h2 id="${item.name.toLowerCase()}" class="item-name">${item.name}</h2>
+    //             <button>remove</button>
+    //             <h4 class="price">$${item.price}</h4>
+    //         </li>
+    //     `
+    // })
+    // document.querySelector('#items-ordered').innerHTML = preCheckoutList
+}
